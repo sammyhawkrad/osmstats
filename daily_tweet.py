17fb78a4f7ab05520fdf9bf9abe7b7346ac9c08e
Samuel Darkwah Manu <manusam14@yahoo.com>
2024-11-20 12:43:01 +0100

    Add daily tweet functionality and web scraping for OpenStreetMap stats
    - Implemented daily_tweet.py to format and post daily statistics on Twitter.
    - Created get_planet_stats.py to scrape and retrieve data from the OpenStreetMap statistics page.
    - Added .gitignore to exclude unnecessary files and directories.

---
+++ daily_tweet.py
@@ -0,0 +1,50 @@
+import tweepy
+import os
+
+from get_planet_stats import get_planet_stats
+
+def format_number(number):
+    return f"{int(number):,}"
+
+report_run_at, report, number_of_editors, top_users = get_planet_stats()
+top_users = top_users['Day']
+
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+
+client = tweepy.Client(
+    consumer_key=consumer_key, consumer_secret=consumer_secret,
+    access_token=access_token, access_token_secret=access_token_secret
+)
+
+tweet_text = f"""
+OSM Daily Stats 📊
+
+Number of users: {format_number(report['Number of users'])} 👥
+
+Number of users that...
+edited nodes: {format_number(number_of_editors[1]['Day'])} ✏️
+uploaded GPX files: {format_number(number_of_editors[0]['Day'])} 🗺️
+
+Top 10 editors:
+1. {top_users[0][1]} - {format_number(top_users[0][0])} 🥇
+2. {top_users[1][1]} - {format_number(top_users[1][0])} 🥈
+3. {top_users[2][1]} - {format_number(top_users[2][0])} 🥉
+4. {top_users[3][1]} - {format_number(top_users[3][0])}
+5. {top_users[4][1]} - {format_number(top_users[4][0])}
+6. {top_users[5][1]} - {format_number(top_users[5][0])}
+7. {top_users[6][1]} - {format_number(top_users[6][0])}
+8. {top_users[7][1]} - {format_number(top_users[7][0])}
+9. {top_users[8][1]} - {format_number(top_users[8][0])}
+10. {top_users[9][1]} - {format_number(top_users[9][0])}
+
+{report_run_at}
+
+#OpenStreetMap #OSM #OpenData #OSMstats
+"""
+
+#response = client.create_tweet(text=tweet_text)
+
+print(tweet_text)
---
+++ get_planet_stats.py
@@ -0,0 +1,69 @@
+import requests  
+from bs4 import BeautifulSoup  
+
+# URL of stats page  
+url = 'https://planet.openstreetmap.org/statistics/data_stats.html'  
+
+# Send a GET request to fetch the content of the webpage  
+response = requests.get(url)  
+html_content = response.text  
+
+# Parse the HTML content using BeautifulSoup  
+soup = BeautifulSoup(html_content, 'html.parser')
+
+REPORT_RUN_AT = soup.find('h2').get_text(strip=True)
+REPORT = {}
+NUMBER_OF_EDITORS = []
+TOP_USERS = {}
+
+
+# Get report run details
+table = soup.find('table')
+for row in table.find_all('tr'):
+    cells = row.find_all('td')
+    if cells:
+        REPORT[cells[0].get_text(strip=True)] = cells[1].get_text(strip=True)
+
+# Get number of users editing over the past day, week, month
+h2_number_of_editors = soup.find('h2', id='number-of-editors')
+if h2_number_of_editors:
+    table_number_of_editors = h2_number_of_editors.find_next('table')
+    headings_number_of_editors = [th.get_text(strip=True) for th in table_number_of_editors.find('tr').find_all('th')]
+    for row in table_number_of_editors.find_all('tr')[1:]:
+        cells = row.find_all(recursive=False)
+        if cells:
+            NUMBER_OF_EDITORS.append({headings_number_of_editors[i]: cells[i].get_text(strip=True) for i in range(len(cells))})
+
+# Get top users editing over the past day, week, month
+h2_top_users = soup.find('h2', id='top-editors')
+table_top_users = h2_top_users.find_next('table')
+
+if table_top_users:
+    table_data = []
+    headings_top_users = [th.get_text(strip=True) for th in table_top_users.find_all('th')]
+
+    for idx, row in enumerate(table_top_users.find_all('tr')[1:]):
+        if idx >= 10:
+            break
+        cells = row.find_all('td')
+        if cells:
+            row_data = {}
+            for i, cell in enumerate(cells):
+                if i < len(headings_top_users):
+                    row_data[headings_top_users[i]] = cell.get_text(strip=False).split(' ')
+            
+            if row_data:
+                table_data.append(row_data)
+    
+    TOP_USERS = {th: [] for th in headings_top_users}
+    TOP_USERS = {th: [row.get(th, '') for row in table_data] for th in headings_top_users}
+    
+def get_planet_stats():
+    return REPORT_RUN_AT, REPORT, NUMBER_OF_EDITORS, TOP_USERS
+
+if __name__ == "__main__":
+    report_run_at, report, number_of_editors, top_users = get_planet_stats()
+    print("Report Run At:", report_run_at)
+    print("Report:", report)
+    print("Number of Editors:", number_of_editors)
+    print("Top Users:", top_users)
